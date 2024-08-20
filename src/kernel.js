73049666080890820a1933a25e9a34b25b8587d4
Sam Harris <harrissam95@gmail.com>
2024-08-20 15:50:31 -0400

    adding spookiness to 127.0.0.1

--- src/kernel.js
+++ src/kernel.js
@@ -41,7 +41,7 @@ function setHeader( msg ) {
     const imgSize = serverDatabase.iconSize || 100;
     const header = `
     <img src="${ imgUrl }" width="${ imgSize }" height="${ imgSize }"
-         style="float: left; padding-right: 10px" class="${ serverDatabase.iconClass || "" }">
+         style="float: left; padding-right: 10px" title="${ serverDatabase.iconTitle || ""}" class="${ serverDatabase.iconClass || "" }">
     <h2 style="letter-spacing: 4px">${ serverDatabase.serverName }</h2>
     <p>Logged in: ${ serverDatabase.serverAddress } (&nbsp;${ dateStr }&nbsp;) </p>
     ${ serverDatabase.headerExtraHTML || "" }
