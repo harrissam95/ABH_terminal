674650115007d82864a47e6faa55c5d2808d1e2d
Sam Harris <harrissam95@gmail.com>
2024-08-20 10:59:51 -0400

    disabled software.js functions, testing removing of help functions

--- config/software.js
+++ config/software.js
@@ -9,35 +9,35 @@
  */
 /* eslint-disable no-inner-declarations, no-nested-ternary, no-sequences, no-unused-vars */
 
-function decrypt( args ) { // The same function can be used to encode text
-    if ( args.length === 0 ) {
-        return "<p>Some encrypted text must be provided: <code>decrypt 53CR3T T3XT</code></p>";
-    }
-    const textInClear = rot13( args.join( " " ) );
-    return `<p class="hack-reveal">${ textInClear }</p>`;
-}
-function rot13( s ) { // cf. https://en.wikipedia.org/wiki/ROT13
-    return s.replace( /[a-zA-Z]/g, ( c ) => String.fromCharCode( ( c <= "Z" ? 90 : 122 ) >= ( c = c.charCodeAt( 0 ) + 13 ) ? c : c - 26 ) );
-}
+// function decrypt( args ) { // The same function can be used to encode text
+//     if ( args.length === 0 ) {
+//         return "<p>Some encrypted text must be provided: <code>decrypt 53CR3T T3XT</code></p>";
+//     }
+//     const textInClear = rot13( args.join( " " ) );
+//     return `<p class="hack-reveal">${ textInClear }</p>`;
+// }
+// function rot13( s ) { // cf. https://en.wikipedia.org/wiki/ROT13
+//     return s.replace( /[a-zA-Z]/g, ( c ) => String.fromCharCode( ( c <= "Z" ? 90 : 122 ) >= ( c = c.charCodeAt( 0 ) + 13 ) ? c : c - 26 ) );
+// }
 
-function identify() {
-    const introMsg = [ "What is this?", `<img src="https://thisartworkdoesnotexist.com/?${ performance.now() }" style="width: 10rem; max-width: 100%;">` ];
-    return { message: introMsg, onInput( answer ) {
-        return `Wrong! This is not "${ answer }"`;
-    } };
-}
+// function identify() {
+//     const introMsg = [ "What is this?", `<img src="https://thisartworkdoesnotexist.com/?${ performance.now() }" style="width: 10rem; max-width: 100%;">` ];
+//     return { message: introMsg, onInput( answer ) {
+//         return `Wrong! This is not "${ answer }"`;
+//     } };
+// }
 
-function artifact( args ) {
-    if ( args.length === 0 ) {
-        return [ "<p>An ID must be provided: <code>artifact $id</code></p>", `You currently have access to the following artifacts: ${ Object.keys( DWEETS ).join( " " ) }` ];
-    }
-    const artifactId = args[ 0 ];
-    const artifactDweet = DWEETS[ artifactId ];
-    if ( !artifactDweet ) {
-        return `You do not have access to the artifact with ID ${ artifactId }`;
-    }
-    return artifactDweet();
-}
+// function artifact( args ) {
+//     if ( args.length === 0 ) {
+//         return [ "<p>An ID must be provided: <code>artifact $id</code></p>", `You currently have access to the following artifacts: ${ Object.keys( DWEETS ).join( " " ) }` ];
+//     }
+//     const artifactId = args[ 0 ];
+//     const artifactDweet = DWEETS[ artifactId ];
+//     if ( !artifactDweet ) {
+//         return `You do not have access to the artifact with ID ${ artifactId }`;
+//     }
+//     return artifactDweet();
+// }
 
 const DWEETS = {
     888: () => dweet( ( t, x ) => { // FROM: https://www.dwitter.net/d/888
